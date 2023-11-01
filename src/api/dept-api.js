34f7019dba81b2f10426cf758bd8d1ef19ce5cce
zzx-dog <2665418341@qq.com>
2023-11-01 16:18:01 +0800

    new

--- src/api/dept-api.js
+++ src/api/dept-api.js
@@ -1,5 +1,19 @@
 import http from "@/api/http";
-
+// 创建新部门
+export function creatDeptAccount(apiObj){
+    return http.post('/dept/v3/department', apiObj)
+}
+// 获取部门列表
 export function getDeptList(pageSize, offset){
     return http.get('/dept/v1/department/' + pageSize + '/' + offset)
+}
+
+//获取指定部门信息
+export function getDeptInfo(dept_id){
+    return http.get('/dept/v1/department/' + dept_id)
+}
+
+//删除部门
+export function deleteDeptByDept_id(dept_id){
+    return http.delete('/dept/v3/department/' + dept_id)
 }
